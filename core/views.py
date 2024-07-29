70c3982638d3649b0598565a7aab16a876dfdb34
Joseph Soper <117962686+joseph-soper@users.noreply.github.com>
2024-07-29 03:44:33 -0400

    Added API views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Booking, Guest, Invoice, Room
+from .serializers import BookingSerializer, GuestSerializer, InvoiceSerializer, RoomSerializer
 
 # Create your views here.
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
