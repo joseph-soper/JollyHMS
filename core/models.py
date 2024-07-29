3912c4adb8a06073911e82b3160f87f40669f25d
Joseph Soper <117962686+joseph-soper@users.noreply.github.com>
2024-07-29 01:48:00 -0400

    Added Room model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    ROOM_TYPES = (
+        ('Q', 'Single Queen'),
+        ('K', 'Single King'),
+        ('QD', 'Double Queen'),
+        ('KD', 'Double King'),
+        ('QS', 'Queen Suite'),
+        ('KS', 'King Suite'),
+    )
+    number = models.CharField(max_length=5, unique=True)
+    room_type = models.CharField(max_length=2, choices=ROOM_TYPES)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Room {self.number} ({self.room_type})"
